0022adb7cd9b7d4f8ccbade878beb34be4142b66
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-11 16:25:36 +0200

    prepare 1.21.10 port

---
+++ Common/src/main/java/fuzs/fallingleavesplus/client/particle/settings/FallingLeavesManager.java
@@ -0,0 +1,85 @@
+package fuzs.fallingleavesplus.client.particle.settings;
+
+import fuzs.fallingleavesplus.FallingLeavesPlus;
+import fuzs.fallingleavesplus.config.ClientConfig;
+import fuzs.fallingleavesplus.core.particles.FallingLeavesParticleOption;
+import fuzs.fallingleavesplus.init.ModRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.FileToIdConverter;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.util.ParticleUtils;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public final class FallingLeavesManager extends SimpleJsonResourceReloadListener<ParticleSettings> {
+    public static final String ASSET_DIRECTORY = FallingLeavesPlus.MOD_ID + "/leaves";
+    private static final FileToIdConverter ASSET_LISTER = FileToIdConverter.json(ASSET_DIRECTORY);
+
+    @Nullable
+    private static FallingLeavesManager instance;
+    private Map<Block, ParticleSettings> particleSettings = Map.of();
+
+    FallingLeavesManager() {
+        super(ParticleSettings.CODEC, ASSET_LISTER);
+    }
+
+    public static void onAddResourcePackReloadListeners(BiConsumer<ResourceLocation, PreparableReloadListener> consumer) {
+        consumer.accept(FallingLeavesPlus.id("falling_leaves_manager"), instance = new FallingLeavesManager());
+    }
+
+    @Override
+    protected void apply(Map<ResourceLocation, ParticleSettings> object, ResourceManager resourceManager, ProfilerFiller profiler) {
+        IdentityHashMap<Block, ParticleSettings> map = new IdentityHashMap<>();
+        for (Map.Entry<ResourceLocation, ParticleSettings> entry : object.entrySet()) {
+            if (BuiltInRegistries.BLOCK.containsKey(entry.getKey())) {
+                map.put(BuiltInRegistries.BLOCK.getValue(entry.getKey()), entry.getValue());
+            }
+        }
+        this.particleSettings = Collections.unmodifiableMap(map);
+    }
+
+    public static ParticleSettings getParticleSettings(BlockState blockState) {
+        return getParticleSettings(blockState.getBlock());
+    }
+
+    public static ParticleSettings getParticleSettings(Block block) {
+        FallingLeavesManager fallingLeavesManager = instance;
+        if (fallingLeavesManager != null) {
+            return instance.particleSettings.getOrDefault(block, ParticleSettings.DEFAULT);
+        } else {
+            return ParticleSettings.DEFAULT;
+        }
+    }
+
+    public static void makeFallingLeavesParticles(Level level, BlockState blockState, BlockPos blockPos, RandomSource randomSource) {
+        ParticleSettings particleSettings = getParticleSettings(blockState);
+        if (!(randomSource.nextFloat() >= particleSettings.getLeafParticleChanceWithWeather(level))) {
+            BlockState blockStateBelow = level.getBlockState(blockPos.below());
+            if (!Block.isFaceFull(blockStateBelow.getCollisionShape(level, blockPos.below()), Direction.UP)) {
+                BlockState blockStateAbove = level.getBlockState(blockPos.above());
+                ParticleOptions particleOptions = new FallingLeavesParticleOption(ModRegistry.FALLING_LEAVES_PARTICLE_TYPE.value(),
+                        particleSettings.getSpawnSnowflakes() &&
+                                FallingLeavesPlus.CONFIG.get(ClientConfig.class).snowflakesSpawningBlocks.contains(
+                                        blockStateAbove.getBlock()) ? blockStateAbove.getBlock().defaultBlockState() :
+                                blockState,
+                        level.getClientLeafTintColor(blockPos));
+                ParticleUtils.spawnParticleBelow(level, blockPos, randomSource, particleOptions);
+            }
+        }
+    }
+}
---
+++ Common/src/main/java/fuzs/fallingleavesplus/mixin/client/LeavesBlockMixin.java
@@ -0,0 +1,29 @@
+package fuzs.fallingleavesplus.mixin.client;
+
+import fuzs.fallingleavesplus.FallingLeavesPlus;
+import fuzs.fallingleavesplus.config.ClientConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LeavesBlock.class)
+abstract class LeavesBlockMixin extends Block {
+
+    public LeavesBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Inject(method = "makeFallingLeavesParticles", at = @At("HEAD"), cancellable = true)
+    private void makeFallingLeavesParticles(Level level, BlockPos blockPos, RandomSource randomSource, BlockState blockState, BlockPos blockPos2, CallbackInfo callback) {
+        if (FallingLeavesPlus.CONFIG.get(ClientConfig.class).defaultLeavesBlocks.contains(this)) {
+            callback.cancel();
+        }
+    }
+}
